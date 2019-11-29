5af2b9ea8d2e1cd4aa1bec412bb04dba0271f8f4
Moreliaa <36938007+Moreliaa@users.noreply.github.com>
2019-11-29 12:34:33 +0100

    add sln

---
+++ AoC2019/AoC2019.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Day1.cpp"
+
+int main()
+{
+	string output = "Text";
+	cout << output << endl;
+	Day1::run();
+	system("pause");
+	return 0;
+}
+
---
+++ AoC2019/Day1.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+class Day1 {
+public:
+	static void run() {
+		cout << "Test1" << endl;
+	}
+};
+
