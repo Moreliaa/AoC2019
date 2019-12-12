e97f82d1946c23398d235e3039120a3931e821b7
Moreliaa <36938007+Moreliaa@users.noreply.github.com>
2019-12-12 19:58:12 +0100

    Pt2 start

--- AoC2019/Day10.cpp
+++ AoC2019/Day10.cpp
@@ -4,58 +4,77 @@
 #include <algorithm>
 
 class Day10 {
+	class Point {
+	public:
+		int x;
+		int y;
+
+		Point() {
+			this->x = 0;
+			this->y = 0;
+		}
+		Point(int x, int y) {
+			this->x = x;
+			this->y = y;
+		}
+	};
 
 	static string createKey(unsigned x, unsigned y) {
 		return to_string(x) + "," + to_string(y);
 	}
 
-	static bool checkDir(map<string, string> &field, unsigned x1, unsigned y1, unsigned x2, unsigned y2, int stepX, int stepY) {
-		int xNext = x1 + stepX;
-		int yNext = y1 + stepY;
+	static bool checkDir(map<string, string> &field, unsigned x1, unsigned y1, unsigned x2, unsigned y2, Point &step) {
+		int xNext = x1 + step.x;
+		int yNext = y1 + step.y;
 		int xMin = min(x1, x2);
 		int xMax = max(x1, x2);
 		int yMin = min(y1, y2);
 		int yMax = max(y1, y2);
 
-		while ((xNext > xMin && xNext < xMax && stepX != 0) || (yNext > yMin && yNext < yMax && stepY != 0))
+		while ((xNext > xMin && xNext < xMax && step.x != 0) || (yNext > yMin && yNext < yMax && step.y != 0))
 		{
 			if (field[createKey(xNext, yNext)] == "#")
 				return false;
-			xNext = xNext + stepX;
-			yNext = yNext + stepY;
+			xNext = xNext + step.x;
+			yNext = yNext + step.y;
 		}
 		return true;
 	}
 
-	static void reduce(int &greater, int &lesser) {
-		int next = abs(greater);
-		int ggt = abs(lesser);
-		int nextop = next % ggt;
-		while (nextop != 0) {
-			next = ggt;
-			ggt = nextop;
-			nextop = next % ggt;
+	static Point getStepSize(unsigned x1, unsigned y1, unsigned x2, unsigned y2) {
+		int stepX = x2 - x1;
+		int stepY = y2 - y1;
+		if (stepY > 1 && stepX < -1)
+			int a = 0;
+		if (stepX == 0)
+			stepY /= abs(stepY);
+		else if (stepY == 0)
+			stepX /= abs(stepX);
+		else {
+			int abs1 = abs(stepX);
+			int abs2 = abs(stepY);
+			int next = max(abs1, abs2);
+			int ggt = min(abs1, abs2);
+			int nextop = next % ggt;
+			while (nextop != 0) {
+				next = ggt;
+				ggt = nextop;
+				nextop = next % ggt;
+			}
+			stepX /= ggt;
+			stepY /= ggt;
 		}
-		greater /= ggt;
-		lesser /= ggt;
+		return Point(stepX, stepY);
 	}
 
 	static bool checkLos(map<string, string> &field, unsigned x1, unsigned y1, unsigned x2, unsigned y2) {
-			int stepX = x2 - x1;
-			int stepY = y2 - y1;
-			if (stepY > 1 && stepX < -1)
-				int a = 0;
-			if (stepX == 0)
-				stepY /= abs(stepY);
-			else if (stepY == 0)
-				stepX /= abs(stepX);
-			else
-				abs(stepX) > abs(stepY) ? reduce(stepX, stepY) : reduce(stepY, stepX);
-			return checkDir(field, x1, y1, x2, y2, stepX, stepY);
+			Point step = getStepSize(x1, y1, x2, y2);
+			return checkDir(field, x1, y1, x2, y2, step);
 	}
 
-	static void mapLosForAst(map<string, string> &field, map<string, int> &losCount, unsigned width, unsigned height, unsigned x, unsigned y, string printTarget) {
+	static map<string, Point> mapSlopeAndLosForAst(map<string, string> &field, map<string, int> &losCount, unsigned width, unsigned height, unsigned x, unsigned y, string printTarget) {
 		string targetKey = createKey(x, y);
+		map<string, Point> slopeMap;
 		bool print = targetKey.compare(printTarget) == 0;
 		for (unsigned y1 = 0; y1 < height; y1++)
 		{
@@ -69,6 +88,7 @@ class Day10 {
 				string key = createKey(x1, y1);
 				if (field[key] == "#") {
 					if (checkLos(field, x, y, x1, y1)) {
+						slopeMap[key] = getStepSize(x, y, x1, y1);
 						losCount[targetKey] += 1;
 						if (print)
 							cout << "*";
@@ -81,6 +101,7 @@ class Day10 {
 			if (print)
 				cout << endl;
 		}
+		return slopeMap;
 	}
 
 	static void mapLos(map<string, string> &field, map<string, int> &losCount, unsigned width, unsigned height, string printTarget="") {
@@ -92,8 +113,8 @@ class Day10 {
 				bool print = key.compare(printTarget) == 0;
 				if (field[key] == "#") {
 					if (print)
-						cout << "===" << key << endl;
-					mapLosForAst(field, losCount, width, height, x, y, printTarget);
+						cout << "===" << key << endl; 
+					mapSlopeAndLosForAst(field, losCount, width, height, x, y, printTarget);
 					if (print)
 						cout << "===" << endl;
 				}
@@ -122,7 +143,7 @@ public:
 		}
 		unsigned width = fileContent[0].length();
 		unsigned height = j;
-		mapLos(field, losCount, width, height, "6,3");
+		//mapLos(field, losCount, width, height);
 		int maximum = 0;
 		string maxKey = "";
 		auto it = losCount.begin();
@@ -134,6 +155,40 @@ public:
 			it++;
 		} while (it != losCount.end());
 		cout << maxKey << " -> Max: " << maximum << endl;
-		// 19, 11
+		int x = 19;
+		int y = 11;
+		map<string, Point> &slopeMap = mapSlopeAndLosForAst(field, losCount, width, height, x, y, "19,11");
+		map<double, string> atanValues;
+		auto it2 = slopeMap.begin();
+		do {
+			Point p = it2->second;
+			double atan = atan2(p.x, p.y) * 180 / 3.141592653;
+			atanValues[atan] = it2->first;
+			it2++;
+		} while (it2 != slopeMap.end());
+
+		int count = 0;
+		auto it3 = atanValues.begin();
+		int ast200idx;
+		do {
+			int angle = (int)it3->first;
+			if (angle == 90) {
+				ast200idx = (atanValues.size() + count) % atanValues.size();
+				break;
+			}
+			it3++;
+			count++;
+		} while (it3 != atanValues.end());
+
+		count = 0;
+		it3 = atanValues.begin();
+		do {
+			if (count == ast200idx) {
+				cout << "Result Pt2: " << it3->second << endl;
+			}
+			
+			it3++;
+			count++;
+		} while (it3 != atanValues.end());
 	}
 };
